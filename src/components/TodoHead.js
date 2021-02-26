21abd175d4a5928c7d3e535a2c53f6cc8cc332fd
박종현 <parkjonghyun@bagjonghyeon-ui-MacBookPro.local>
2021-02-26 19:13:19 +0900

    first commit

---
+++ src/components/TodoHead.js
@@ -0,0 +1,51 @@
+import React from "react";
+import styled from "styled-components";
+import { useTodoState } from "../TodoContext"; // export default 로 내보내면 그냥 함수 이름으로 써도 되지만 default가 빠지면 {함수이름} 으로 불러와야한다
+
+const TodoHeadBlock = styled.div`
+  padding-top: 48px;
+  padding-left: 32px;
+  padding-right: 32px;
+  padding-bottom: 24px;
+  border-bottom: 1px solid #e9ecef;
+  h1 {
+    margin: 0;
+    font-size: 36px;
+    color: #343a40;
+  }
+  .day {
+    margin-top: 4px;
+    color: #868e96;
+    font-size: 21px;
+  }
+  .tasks-left {
+    color: #20c997;
+    font-size: 18px;
+    margin-top: 40px;
+    font-weight: bold;
+  }
+`;
+
+function TodoHead() {
+  const todos = useTodoState();
+  //console.log(todos);
+  const undoneTasks = todos.filter((todo) => !todo.done);
+
+  const today = new Date();
+  const dateString = today.toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  const dayName = today.toLocaleDateString("ko-KR", { weekday: "long" });
+
+  return (
+    <TodoHeadBlock>
+      <h1>{dateString}</h1>
+      <div className="day">{dayName}</div>
+      <div className="tasks-left">할 일 {undoneTasks.length}개 남음</div>
+    </TodoHeadBlock>
+  );
+}
+
+export default TodoHead;
